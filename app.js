9fdccbb0d17c454dea457203d5114b8a4838ae9a
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-01-05 00:30:58 -0800

    Day3-Lab HomeWork

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
 var player = prompt('Welcome to my mini Game! Please Enter Your Name to Continue...');
 if (player)
   alert('Hello' + ' ' + player + '! My name is Luay, I will ask you some random questions about myself. If you get all of them correct, you will receive a gift from me ;-)');
+
 var rightAnswers = 0;
 
 //Question 1
 var questionOne = prompt('What is my favorite Color?').toUpperCase();
 if (questionOne === 'GREEN'){
-  alert('Correct! I am a Ninja Turtle. :-)');
-  var answer1 = 'Correct! I am a Ninja Turtle. :-)';
-  rightAnswer++;
+  alert('Sure! I am a Ninja Turtle. :-)');
+  var answer1 = 'Sure! I am a Ninja Turtle. :-)';
+  rightAnswers++;
 } else {
   alert('Wrong Answer. My favorite color is Green because I am a Ninja Turtle.');
   var answer1 = 'Wrong Answer. My favorite color is Green because I am a Ninja Turtle.';
@@ -19,45 +20,45 @@ if (questionOne === 'GREEN'){
 //Question 2
 var questionTwo = prompt('Which one do you think I love more, Pizza or Burgers?').toUpperCase();
 if (questionTwo === 'PIZZA') {
-  alert('Right, that\s obvious, all Ninja Turtles love Pizza.');
-  var answer2 = 'Right, that\s obvious, all Ninja Turtles love Pizza.';
-  rightAnswer++;
+  alert('Right, that\'s obvious, all Ninja Turtles love Pizza. :P');
+  var answer2 = 'Right, that\'s obvious, all Ninja Turtles love Pizza. :P';
+  rightAnswers++;
 } else {
-  alert('Sure! who doesn\'t!');
-  var answer2 = 'Sure! who doesn\'t!';
+  alert('Nope, Ninja Turtles don\'t eat Burgers.');
+  var answer2 = 'Nope, Ninja Turtles don\'t eat Burgers.';
 }
 
 //Question 3
-var questionThree = prompt('I was born in January but my Passport says April! Truth or Lie?').toUpperCase();
+var questionThree = prompt('I was born in January but my Passport says April! Truth or Lie? Hint: Don\'t be fooled by April Fools\' Day').toUpperCase();
 if (questionThree === 'YES' || questionThree === 'Y' || questionThree === 'YEP' || questionThree === 'YEAH' || questionThree === 'TRUTH'){
-  alert('Right, and I can\'t change it because my national ID was updated to that as well! SMH, Embassy Mistakes.');
-  var answer3 = 'Right, and I can\'t change it because my ID was updated to that as well! Embassy Mistakes.';
-  rightAnswer++;
+  alert('Right, and I can\'t change it because my national ID was updated to the wrong one as well! SMH, Embassy Mistakes.');
+  var answer3 = 'Right, and I can\'t change it because my national ID was updated to the wrong one as well! SMH, Embassy Mistakes.';
+  rightAnswers++;
 } else {
   alert('Well, I am afraid you are wrong. Our embassy makes awful mistakes.');
   var answer3 = 'Well, I am afraid you are wrong. Our embassy makes awful mistakes.';
 }
 
 //Question 4
-var questionFour = prompt('My best friend\'s name start with the letter Q').toUpperCase();
-if (questionFour === 'YES' || questionFour === 'Y' || questionFour === 'YEP' || questionFour === 'YEAH'){
-  alert('You are right! His name is Qusay.');
-  var answer4 = 'You are right! His name is Qusay.';
-  rightAnswer++;
+var questionFour = prompt('Two years ago, I had a Nissan but I drove Audi').toUpperCase();
+if (questionFour === 'RIGHT' || questionFour === 'YES' || questionFour === 'REALLY' || questionFour === 'TRUTH'){
+  alert('You are right! My Boss has an A4 Audi that he doesn\'t like. He did let me drive it for 6 months for free');
+  var answer4 = 'You are right! My old Boss has an A4 Audi that he doesn\'t like. He did let me drive it for 6 months for free';
+  rightAnswers++;
 } else {
-  alert('False. My friend\'s name is Qusay.');
-  var answer4 = 'False. My friend\'s name is Qusay.';
+  alert('I know it sounds hard to believe but my old Boss has an A4 Audi that he doesn\'t like. He did let me drive it for 6 months for free');
+  var answer4 = 'I know it sounds hard to believe but my old Boss has an A4 Audi that he doesn\'t like. He did let me drive it for 6 months for free';
 }
 
 //Question 5
 var questionFive = prompt('Do you think I\'m a Dog person?').toUpperCase();
 if (questionFive === 'YES' || questionFive === 'Y' || questionFive === 'YEP' || questionFive === 'YEAH'){
-  alert('YEP, but I love rabbits more.');
-  var answer5 = 'YEP, but I love rabbits more.';
-  rightAnswer++;
+  alert('YES! I love Dogs! I had Alaskan Husky back home and never was able to walk him outside the house because of the Hot Weather.');
+  var answer5 = 'YES! I love Dogs! I had Alaskan Husky back home and never was able to walk him outside the house because of the Hot Weather.';
+  rightAnswers++;
 } else {
-  alert('I\'m Sorry, but I love Dogs.');
-  var answer5 = 'I\'m Sorry, but I love Dogs.';
+  alert('Incorrect. I love Dogs. I had Alaskan Husky back home and never was able to walk him outside the house because of the Hot Weather.');
+  var answer5 = 'Incorrect. I love Dogs. I had Alaskan Husky back home and never was able to walk him outside the house because of the Hot Weather.';
 }
 
 //Question 6
@@ -77,14 +78,14 @@ for (var i = 0;i < 3;i++){
     counter++;
   } else if (questionSix == 10){
     questionSix = alert('Good Guess! I have visited' + ' ' + coutnriesArray);
-    rightAnswer++;
+    rightAnswers++;
   }
 }
 if (questionSix != 10){
   alert('Wrong answer. I have visited 10 countries excluding my home country.' + ' ' + coutnriesArray + '.');
 }
 
-document.write('<p>' + 'So' + ' ' + player + ', you have' + ' ' + rightAnswer + ' ' + 'out of 7 correct answers. Here is the transcript of the original questions, your answers, and the comments that were displayed to you.');
+document.write('<p>' + 'So' + ' ' + player + ', you have' + ' ' + rightAnswers + ' ' + 'out of 7 correct answers. Here is the transcript of the original questions, your answers, and the comments that were displayed to you.');
 document.write('<p>' + 'Q1: What is my favorite Color?' + ' ' + questionOne + ' ' + '|' + ' ' + 'A:' + ' ' + answer1 + '</p>');
 document.write('<p>' + 'Q2: Which one do you think I love more, Pizza or Burgers?' + ' ' + questionTwo + ' ' + '|' + ' ' + 'A:' + ' ' + answer2 + '</p>');
 document.write('<p>' + 'Q3: I was born in January but my Passport says April! Truth or Lie?' + ' ' + questionThree + ' ' + '|' + ' ' + 'A:' + ' ' + answer3 + '</p>');
